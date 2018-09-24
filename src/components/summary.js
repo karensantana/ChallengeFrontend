758dda6ada259b17695381801354468c1fabc2e7
karensantana <itt.karensantana@gmail.com>
2018-09-23 21:46:50 -0400

    Adding summary info

--- src/components/summary.js
+++ src/components/summary.js
@@ -9,6 +9,27 @@ const Summary =() => {
                 <div className="info-box">
                     <h4 className="details-header">Sending Details</h4>
                 </div>
+                <div className="info-box info-box-middle white-box">
+                        <h4 className="summary-info">You send</h4>
+                    </div>
+                <div className="info-box info-box-middle">
+                    <h4 className="details-header">Receiving Details</h4>
+                </div>
+                <div className="receiving-details info-box-middle white-box side-border">
+                    <h4 className="sumary-info">Rate</h4>
+                </div>
+                <div className="receiving-details info-box-middle white-box side-border">
+                    <h4 className="sumary-info">Fee</h4>
+                </div>
+                <div className="receiving-details info-box-middle white-box side-border">
+                    <h4 className="sumary-info">Delivery date</h4>
+                </div>
+                <div className="receiving-details info-box-middle white-box side-border">
+                    <h4 className="sumary-info">Recipient gets</h4>
+                </div>
+                <div className="info-box">
+                    <h4 className="saving-info">You save compared to your bank!</h4>
+                </div>
          </div>
         </div>
     );
