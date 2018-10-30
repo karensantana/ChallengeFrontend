99524dea685d061fe1ec5006f44718ba8039accb
Karen Santana <itt.karensantana@gmail.com>
2018-10-30 14:16:34 -0400

    Adding affix to Summary Box

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,6 @@
 import { SHOW_MODAL } from "../constants/action-types";
 import { HIDE_MODAL } from "../constants/action-types";
 
-export const showModal = amountToSend => ({
-    type: SHOW_MODAL,
-    payload: amountToSend
-});
 
-export const hideModal = amountToSend => ({
-    type: HIDE_MODAL,
-    payload: amountToSend
-});
 
 
